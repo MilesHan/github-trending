80e82494fe646734ef9bb9ec1aa9de5cd896c149
Handa <ignatius102@qq.com>
2024-11-27 17:19:33 +0800

    add weekly monthly

---
+++ scraper-monthly.py
@@ -0,0 +1,103 @@
+# coding:utf-8
+
+import datetime
+import codecs
+import requests
+import os
+import time
+from pyquery import PyQuery as pq
+
+
+def git_add_commit_push(date, filename):
+    cmd_git_add = 'git add {filename}'.format(filename=filename)
+    cmd_git_commit = 'git commit -m "{date}"'.format(date=date)
+    cmd_git_push = 'git push -u origin master'
+
+    os.system(cmd_git_add)
+    os.system(cmd_git_commit)
+    os.system(cmd_git_push)
+
+
+def createMarkdown(date, filename):
+    with open(filename, 'w') as f:
+        f.write("## " + date + "\n")
+
+
+def scrape(language, filename):
+    HEADERS = {
+        'User-Agent'		: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0',
+        'Accept'			: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding'	: 'gzip,deflate,sdch',
+        'Accept-Language'	: 'zh-CN,zh;q=0.8'
+    }
+
+    url = 'https://github.com/trending/{language}?since=monthly'.format(language=language)
+    r = requests.get(url, headers=HEADERS)
+    assert r.status_code == 200
+    
+    d = pq(r.content)
+    items = d('div.Box article.Box-row')
+
+    # codecs to solve the problem utf-8 codec like chinese
+    with codecs.open(filename, "a", "utf-8") as f:
+        f.write('\n#### {language}\n'.format(language=language))
+
+        for item in items:
+            i = pq(item)
+            title = i(".lh-condensed a").text()
+            owner = i(".lh-condensed span.text-normal").text()
+            description = i("p.col-9").text()
+            url = i(".lh-condensed a").attr("href")
+            url = "https://github.com" + url
+            # ownerImg = i("p.repo-list-meta a img").attr("src")
+            # print(ownerImg)
+            f.write(u"* [{title}]({url}):{description}\n".format(title=title, url=url, description=description))
+
+
+def job():
+    strdate = datetime.datetime.now().strftime('%Y-%m-%d')
+    filename = '{date}-monthly.md'.format(date=strdate)
+
+    # create markdown file
+    createMarkdown(strdate, filename)
+
+    # write markdown
+    scrape('', filename)
+    scrape('javascript', filename)
+    scrape('typescript', filename)
+    scrape('php', filename)
+    scrape('vue', filename)
+    scrape('html+php', filename)
+    scrape('html', filename)
+    scrape('css', filename)
+    scrape('python', filename)
+
+    # Origin Catch Types
+    # scrape('python', filename)
+    # scrape('swift', filename)
+    # scrape('javascript', filename)
+    # scrape('go', filename)
+
+    # git add commit push
+    # git_add_commit_push(strdate, filename)
+
+
+if __name__ == '__main__':
+    job()
+
+# 每月的最后1天
+# 0 0 L * * *   python3 scraper-monthly.py
+
+# 每周日早上6点更新
+# 0 6 * * 0 *   python3 scraper-weekly.py
+
+# 每天早上6点更新
+# 0 6 * * * *   python3 scraper.py
+
+
+19 17 * * * python3 scraper.py
+
+# https://open.itc.cn/  这个网站有收录，但是不好看
+
+
+
